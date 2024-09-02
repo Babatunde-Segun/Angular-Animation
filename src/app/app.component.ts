270afc9901ac1f98ff3596073931a03a86d8c7f1
Babatunde-Segun <officiallyvincify@gmail.com>
2024-09-02 19:07:35 +0100

    animation works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 // import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Component, HostBinding } from '@angular/core';
 import {
   trigger,
@@ -9,14 +10,22 @@ import {
   transition,
   // ...
 } from '@angular/animations';
+import { slideInAnimation } from './animation';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  animations: [slideInAnimation],
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-amination';
+  constructor(private contexts: ChildrenOutletContexts) {}
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
+      'animation'
+    ];
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,22 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { QueryComponent } from './components/query/query.component';
 
 export const routes: Routes = [
-  
+  {
+    path: 'query',
+    component: QueryComponent,
+    data: { animation: 'queryingPage' },
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { animation: 'HomePage' },
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    data: { animation: 'AboutPage' },
+  },
 ];
