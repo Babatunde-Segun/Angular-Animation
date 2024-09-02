6627ce044c281df86d752979e1a839663d8f3662
Babatunde-Segun <officiallyvincify@gmail.com>
2024-09-02 18:46:02 +0100

    initial phase settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-amination';
